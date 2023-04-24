3d4da2ec6031654b03c5c29d27e08c3f67b55fdd
nanami69 <69er.11.11@gmail.com>
2023-04-24 15:52:26 +0900

    feat: register cinema

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type User struct {
@@ -22,10 +24,21 @@ func getUser(c echo.Context) error {
 	return c.JSON(http.StatusOK, user)
 }
 
+func registerCinema(c echo.Context) error {
+	// データベースの登録と緯度経度算出処理
+	return c.JSON(http.StatusOK, map[string]string{
+		"message": "test",
+	})
+}
+
 func main() {
 	e := echo.New()
+
+	// CORSの設定(デプロイ前に設定を見直す)
+	e.Use(middleware.CORS())
 	// ルーティングの設定
 	e.GET("/users/:id", getUser)
+	e.POST("/register-cinema", registerCinema)
 	// サーバーの起動
 	e.Logger.Fatal(e.Start(":8080"))
 }
