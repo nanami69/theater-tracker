f04c9dfac849bafaff185ff2250f2fabab2b190e
nanami69 <69er.11.11@gmail.com>
2023-05-03 10:23:03 +0900

    refactor: Extract the getLatLng function

---
+++ backend/pkg/geocoding_client.go
@@ -0,0 +1,39 @@
+package geocoding
+
+func getLatLng(address string) (string, string, error) {
+    // APIに送信するリクエストを作成
+    apiURL := "https://msearch.gsi.go.jp/address-search/AddressSearch?q=" + url.QueryEscape(address)
+    req, err := http.NewRequest("GET", apiURL, nil)
+    if err != nil {
+        return "", "", err
+    }
+    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+    // リクエストを送信
+    client := new(http.Client)
+    resp, err := client.Do(req)
+    if err != nil {
+        return "", "", err
+    }
+    defer resp.Body.Close()
+
+    // レスポンスをパース
+    var res []struct {
+        Geometry struct {
+            Coordinates []float64 `json:"coordinates"`
+        } `json:"geometry"`
+    }
+    err = json.NewDecoder(resp.Body).Decode(&res)
+    if err != nil {
+        return "", "", err
+    }
+
+    // レスポンスから緯度経度を取得
+    if len(res) == 0 {
+        return "", "", fmt.Errorf("no result")
+    }
+    longitude := fmt.Sprintf("%f", res[0].Geometry.Coordinates[0])
+    latitude := fmt.Sprintf("%f", res[0].Geometry.Coordinates[1])
+
+    return latitude, longitude, nil
+}
